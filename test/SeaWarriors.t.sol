1ca7ce2b25514ff4de7c3dff580ab6a65cc96b6e
odmitriy <shubus.corporation@gmail.com>
2025-12-09 02:06:51 +0200

    Remove extra stuff and add comment for testing

--- test/SeaWarriors.t.sol
+++ test/SeaWarriors.t.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+/*
+  const max = 13;
+  const initial = 1250;
+
+  for (let coeff = 0.1; coeff < 2.0; coeff+=0.1) {
+    let curr = initial;
+    let arr = new Array(max);
+    let summ = 0;
+
+    for (let i=0; i < max; i++) {
+      curr = Math.floor(curr * coeff);
+      arr[i] = curr;
+      summ += curr;
+      
+    }
+
+    for (let i = 0; i < max; i++) {
+      console.log(`${ arr[i] } --> ${ Math.floor(arr[i] * 100 / summ) }% from ${ summ }`);
+
+    }
+    console.log("----------------------------------------");
+  }
+*/
+
 import {Test} from "forge-std/Test.sol";
 import {SeaWarriors} from "src/SeaWarriors.sol";
 
